e53b2efe2ed071c660b41876fe553f0dbebedfa6
Ndeye Bougouma Sy <mamys8931@gmail.com>
2022-10-10 17:31:22 +0200

    modifit

--- liste_employe.php
+++ liste_employe.php
@@ -19,7 +19,7 @@ catch(Exception $e)
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="inclusion/CSS/style.css">
+  <
 
     <title>Liste Employe</title>
 </head>
